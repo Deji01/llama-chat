bde419246c03263a5d2fd5915c14006a6b9bfdeb
Ayodeji Yekeen <ayodejiyou@gmail.com>
2024-11-01 13:08:01 +0000

    fixed CSRF issue

--- ai-chatbot-main/next.config.ts
+++ ai-chatbot-main/next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     ppr: true,
+    serverActions : {
+      // allowedForwardedHosts: ["fearful-spirit-gpj64jx94vr39qjq-3000.app.github.dev"],
+      allowedOrigins: ["fearful-spirit-gpj64jx94vr39qjq-3000.app.github.dev", "localhost:3000"],
+      
+    }
   },
   images: {
     remotePatterns: [
