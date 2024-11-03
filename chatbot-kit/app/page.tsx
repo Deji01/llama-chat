4ca2ab4fb8ab2d5cbad28aed9606d3766126279a
Ayodeji Yekeen <ayodejiyou@gmail.com>
2024-11-03 08:36:03 +0000

    chatbot UI

---
+++ chatbot-kit/app/page.tsx
@@ -0,0 +1,11 @@
+import { ChatDemo } from "@/components/ChatDemo";
+import { ChatWithSuggestions } from "@/components/ChatWithSuggestions";
+import { CustomChat } from "@/components/CustomChat";
+
+export default function Home() {
+  return (
+    <ChatDemo />
+    // <ChatWithSuggestions />
+    // <CustomChat />
+  )
+}
